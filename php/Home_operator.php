e407649ce4fa9ae7eb2fbae6d1f92671772ea706
WimukthiRaj <wimukthirajapaksha.wr@gmail.com>
2017-07-17 05:15:02 +0530

    Designed home pages of teach & operator

--- php/Home_operator.php
+++ php/Home_operator.php
@@ -11,7 +11,7 @@ $operator = new DataOperator($user);
     <meta charset="UTF-8">
 
     <title>Data Operator's Home</title>
-    <link rel="stylesheet" type="text/css" href="../css/teachHome.css"/>
+    <link rel="stylesheet" type="text/css" href="../css/operHome.css"/>
 </head>
 <body>
 <div class = "wrpper">
@@ -19,52 +19,54 @@ $operator = new DataOperator($user);
     <div class = "top-bar">
 
         <h1>Ravi Costructions</h1>
-        <h2>RAVI HEAVY MACHINARY EQUIPMENT TRAINING INSTITUTE</h2
+        <h2>RAVI HEAVY MACHINARY EQUIPMENT TRAINING INSTITUTE</h2>
+    </div>
+    <div class="main">
+        <p><h3><u>Data Operator's Home</u></h3></p>
+        <div class="intro">
+            <div class="introText">
+                <form action="" method="post">
+                    <ul>
+                        <li><button type ="submit" name="viewProfile">VIEW PROFILE</button></li>
+                        <li><button type ="submit" name="editProfile">EDIT PROFILE</button></li>
+                        <li><button type ="submit" name="viewCourseDetails">VIEW COURSE DETAILS</button></li>
+                        <li><button type ="submit" name="enterStudentsAttendence">ENTER STUDENTS' ATTENDANCE</button></li>
+                        <li><button type ="submit" name="createNewAccount">CREATE NEW ACCOUNT</button></li>
+                        <li><button type ="submit" name="enterStudentsMarks">ENTER STUDENTS' MARKS</button></li>
+                        <li><div class="dropdown">
+                            <button class="dropbtn" name="editCourseDetails">EDIT COURSE DETAILS</button>
+                            <div class="dropdown-content">
+                                <button type ="submit" name="BackhoeLoader">Backhoe Loader</button>
+                                <button type ="submit" name="ExcavatorWithBreaker">Excavator With Breaker</button>
+                                <button type ="submit" name="MoterGrader">Moter Grader</button> 
+                            </div>
+                        </div></li>
+                    </ul>
+                </form>
 
-        <div class="main">
-
-            <p><h3><u>Data Operator's Home</u></h3></p>
-
-            <form action="" method="post">
-
-                <button type ="submit" name="viewProfile">VIEW PROFILE</button>
-                <button type ="submit" name="editProfile">EDIT PROFILE</button>
-
-                <div class="dropdown">
-                    <button class="dropbtn" name="editCourseDetails">EDIT COURSE DETAILS</button>
-                    <div class="dropdown-content">
-                        <button type ="submit" name="BackhoeLoader">Backhoe Loader</button>
-                        <button type ="submit" name="ExcavatorWithBreaker">Excavator With Breaker</button>
-                        <button type ="submit" name="MoterGrader">Moter Grader</button> 
-                    </div>
-                </div>
-
-                <button type ="submit" name="viewCourseDetails">VIEW COURSE DETAILS</button>
-                
-                <button type ="submit" name="enterStudentsAttendence">ENTER STUDENTS' ATTENDANCE</button>
-                <button type ="submit" name="createNewAccount">CREATE NEW ACCOUNT</button>
-                <button type ="submit" name="enterStudentsMarks">ENTER STUDENTS' MARKS</button>
-
-            </form>
-
-            <?php  
-            print_r($_POST);
-            if(isset($_POST['viewProfile'])){
-                $operator->viewProfile();
-                // echo "string";
-            }elseif (isset($_POST['BackhoeLoader'])) {
-                header("location: EditCourseDetails.php?usr=".$operator->getName()."&index=".'1');
-            }elseif (isset($_POST['ExcavatorWithBreaker'])) {
-                header("location: EditCourseDetails.php?usr=".$operator->getName()."&index=".'2');
-            }elseif (isset($_POST['MoterGrader'])) {
-                header("location: EditCourseDetails.php?usr=".$operator->getName()."&index=".'3');
-            }elseif (isset($_POST['enterStudentsMarks'])) { 
-                header("location: EnterMark.php?usr=".$operator->getName()."&ty=".'operator');
-            }else{
-                echo "LLLLL";
-            }?>
-
+                <?php  
+                // print_r($_POST);
+                if(isset($_POST['viewProfile'])){
+                    $operator->viewProfile();
+                    // echo "string";
+                }elseif (isset($_POST['BackhoeLoader'])) {
+                    header("location: EditCourseDetails.php?usr=".$operator->getName()."&index=".'1');
+                }elseif (isset($_POST['ExcavatorWithBreaker'])) {
+                    header("location: EditCourseDetails.php?usr=".$operator->getName()."&index=".'2');
+                }elseif (isset($_POST['MoterGrader'])) {
+                    header("location: EditCourseDetails.php?usr=".$operator->getName()."&index=".'3');
+                }elseif (isset($_POST['enterStudentsMarks'])) { 
+                    header("location: EnterMark.php?usr=".$operator->getName()."&ty=".'operator');
+                }else{
+                    // echo "LLLLL";
+                }?>
+            </div>
+            <div class="introImage">
+                <img src="../img/images (2).jpg" alt="your photo"/>
+            </div>
         </div>
+    </div>
+</div>
 </body>
 </html>
 
--- php/Home_teacher.php
+++ php/Home_teacher.php
@@ -31,26 +31,36 @@ if(isset($_POST['viewProfile'])){
     <div class = "top-bar">
 
         <h1>Ravi Costructions</h1>
-        <h2>RAVI HEAVY MACHINARY EQUIPMENT TRAINING INSTITUTE</h2
-
+        <h2>RAVI HEAVY MACHINARY EQUIPMENT TRAINING INSTITUTE</h2>
+    </div>
     <div class="main">
-
         <p><h3><u>Teacher's Home</u></h3></p>
+        <div class="intro">
+            <div class="introText">
+                <form action="" method="post">
+                    <ul>
+                        <li><button type ="submit" name="viewProfile"><p>VIEW PROFILE</p></button></li>
+                        <li><button type ="submit" name="editProfile"><p>EDIT PROFILE</p></button></li>
+                        <li><button type ="submit" name="viewCourseDetails"><p>VIEW COURSE DETAILS</p></button></li>
+                        <li><button type ="submit" name="enterMarks"><p>ENTER STUDENTS' MARKS</p></button></li>
+                        <li><button type ="submit" name="viewStudentReport"><p>VIEW STUDENT'S REPORTS</p></button></li>
+                    </ul>
+                </form>
+                <?php 
+                if (isset($_POST['enterMarks'])) {
+                    header("location: EnterMark.php?usr=".$teacher->getName()."&ty=".'teacher');
+                }
+                ?>
+            </div>
+            <div class="introImage">
+                <img src="../img/images.jpg" alt="your photo"/>
+            </div>
+        </div>
+    </div>
+    <div class="final">
+        <a href="index.php">Log Out</a>
+    </div>
 
-        <form action="" method="post">
-
-            <button type ="submit" name="viewProfile">VIEW PROFILE</button>
-            <button type ="submit" name="editProfile">EDIT PROFILE</button>
-            <button type ="submit" name="viewCourseDetails">VIEW COURSE DETAILS</button>
-            <button type ="submit" name="enterMarks">ENTER STUDENTS' MARKS</button>
-            <button type ="submit" name="viewStudentReport">VIEW STUDENT'S REPORTS</button>
-
-        </form>
-        <?php 
-        if (isset($_POST['enterMarks'])) {
-            header("location: EnterMark.php?usr=".$teacher->getName()."&ty=".'teacher');
-        }
-        ?>
 </div>
 </body>
 </html>
